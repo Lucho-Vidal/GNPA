c2d682d87523c95fccc71ec2814f2503f2113758
luciano <luciano.vidal@trenesargentinos.gob.ar>
2024-12-28 14:47:17 -0300

    agrego rotacion y tren siguiente

--- src/interfaces/Itinerario.ts
+++ src/interfaces/Itinerario.ts
@@ -4,27 +4,12 @@ export interface Itinerario {
     tren: string;
     circular: string;
     itinerario: string;
+    rotacion: string;
+    trenSiguiente: string;
+    trenSiguienteHora:string;
     estaciones: string[];
     horarios: string[];
     horarioXEst:Record<string,string>
     viewDetail:boolean;
-    /* PC: string;
-    ALL: string;
-    RE: string;
-    TY: string;
-    BO: string;
-    GW: string;
-    AK: string;
-    CY: string;
-    FV: string;
-    BQ: string;
-    LLV: string;
-    MG: string;
-    ZZ: string;
-    QL: string;
-    BZ: string;
-    HD: string;
-    VE: string;
-    OA: string;
-    LP: string; */
+
 }
