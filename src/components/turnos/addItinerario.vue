eea5d77edcd3e3d9a853ad700dc81b4ec95fabd2
luciano <luciano.vidal@trenesargentinos.gob.ar>
2024-12-28 14:48:22 -0300

    agrego rotacion y tren siguiente

--- src/components/turnos/addItinerario.vue
+++ src/components/turnos/addItinerario.vue
@@ -48,71 +48,71 @@
         <router-link class="btn btn-danger d-flex end" to="/itinerariosTrenes">Cancelar</router-link>
     </div>
 
-        <table class="table table-striped table-hover">
-        <thead>
-            <tr>
-            <th class="col-1">N°</th>
-            <th class="col-1">Turno</th>
-            <th class="col-1">Itinerario</th>
-            <th class="col-1">Circular</th>
-            <th class="col-1">Borrar</th>
-            </tr>
-        </thead>
-        <tbody
-            v-for="(tren, index) in lstItinerariosTrenes"
-            :key=" index"
-            class="Small shadow"
-            @dblclick="viewDetail(tren)">
-            <tr>
-            <td class="col-1">{{ index + 1 }}</td>
-            <td class="col-1">
-                <input class="form-control" type="text" v-model="tren.tren" />
-            </td>
-            <td class="col-1">
-                <select class="form-control" v-model="tren.itinerario">
-                <option value="H">Hábil</option>
-                <option value="S">Sábado</option>
-                <option value="D">Domingo</option>
-                </select>
-            </td>
-            <td class="col-1">
-                <input class="form-control" type="text" v-model="tren.circular" />
-            </td>
-            <td class="col-1">
-                <i class="material-icons cursor-hand rojo" @click="deleteTurno(index)">delete_forever</i>
-            </td>
-            </tr>
-            <tr v-if="tren.viewDetail" class="details-row">
-                <td colspan="5">
-                    <div class="d-flex flex-nowrap justify-content-center">
-                    <input
-                        v-for="(estacion, idx) in tren.estaciones"
-                        :key="'estacion-' + idx"
-                        class="form-control mx-2 mb-2 col-1"
-                        type="text"
-                        :placeholder="estacion"
-                        v-model="tren.estaciones[idx]"
-                        style="width: 200px;"
-                    />
-                    </div>
-                </td>
-            </tr>
-            <tr v-if="tren.viewDetail" class="details-row">
-                <td colspan="5">
-                    <div class="d-flex flex-nowrap justify-content-center">
-                    <input
-                        v-for="(horario, idx) in tren.horarios"
-                        :key="'estacion-' + idx"
-                        class="form-control mx-2 mb-2 col-1"
-                        type="text"
-                        :placeholder="horario"
-                        v-model="tren.horarios[idx]"
-                        style="width: 200px;"
-                    />
-                    </div>
-                </td>
-            </tr>
-        </tbody>
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                <th class="col-1">N°</th>
+                <th class="col-1">Turno</th>
+                <th class="col-1">Itinerario</th>
+                <th class="col-1">Circular</th>
+                <th class="col-1">Rotacion</th>
+                <th class="col-1">Tren Siguiente</th>
+                <th class="col-1">Hora Tren Siguiente</th>
+                <th class="col-1">Borrar</th>
+                </tr>
+            </thead>
+            <tbody
+                v-for="(tren, index) in lstItinerariosTrenes"
+                :key=" index"
+                class="Small shadow"
+                @dblclick="viewDetail(tren)">
+                <tr>
+                    <td class="col">{{ index + 1 }}</td>
+                    <td class="col-1"><input class="form-control" type="text" v-model="tren.tren" /></td>
+                    <td class="col-1">
+                        <select class="form-control" v-model="tren.itinerario">
+                            <option value="H">Hábil</option>
+                            <option value="S">Sábado</option>
+                            <option value="D">Domingo</option>
+                        </select>
+                    </td>
+                    <td class="col-1"><input class="form-control" type="text" v-model="tren.circular" /></td>
+                    <td class="col-1"><input class="form-control" type="text" v-model="tren.rotacion" /></td>
+                    <td class="col-1"><input class="form-control" type="text" v-model="tren.trenSiguiente" /></td>
+                    <td class="col-1"><input class="form-control" type="text" v-model="tren.trenSiguienteHora" /></td>
+                    <td class="col-1"><i class="material-icons cursor-hand rojo" @click="deleteTurno(index)">delete_forever</i></td>
+                </tr>
+                <tr v-if="tren.viewDetail" class="details-row">
+                    <td colspan="12" >
+                        <div class="d-flex flex-nowrap justify-content-center">
+                        <input
+                            v-for="(estacion, idx) in tren.estaciones"
+                            :key="'estacion-' + idx"
+                            class="form-control mx-2 mb-2 col-1"
+                            type="text"
+                            :placeholder="estacion"
+                            v-model="tren.estaciones[idx]"
+                            style="width: 80px;"
+                        />
+                        </div>
+                    </td>
+                </tr>
+                <tr v-if="tren.viewDetail" class="details-row">
+                    <td colspan="12" class="text-center">
+                        <div class="d-flex flex-nowrap justify-content-center">
+                        <input
+                            v-for="(horario, idx) in tren.horarios"
+                            :key="'estacion-' + idx"
+                            class="form-control mx-2 mb-2 col-1"
+                            type="text"
+                            :placeholder="horario"
+                            v-model="tren.horarios[idx]"
+                            style="width: 80px;"
+                        />
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
         </table>
     </div>
     </main>
@@ -205,7 +205,8 @@ export default defineComponent({
         },
         ConvertirJson() {
             this.sheetNamesSeleccionado.forEach(sheetName => {
-                const {rows,estaciones} = this.obtenerItEstRows(sheetName) as { rows: number[]; estaciones: string[] };
+                const {rows,estaciones,rowRotacion,rowTrenSiguiente,rowTrenSiguienteHora} = 
+                    this.obtenerItEstRows(sheetName) as { rows: number[]; estaciones: string[] ;rowRotacion:number;rowTrenSiguiente:number,rowTrenSiguienteHora:number };
                 if(!rows && !estaciones) return
                 
                 const sheet = this.workbook!.Sheets[sheetName]
@@ -213,6 +214,13 @@ export default defineComponent({
                     console.error(`La hoja ${sheetName} no se pudo leer correctamente.`);
                     alert("ocurrió un error al tratar de leer una hoja, recargue la pagina e intente nuevamente" )
                     return;
+                }
+                const formatoHora = (cell:number)=>{
+                    // Convertir el número a horas y minutos
+                    const totalMinutes = Math.round(cell * 24 * 60); // Convertir días a minutos
+                    const hours = Math.floor(totalMinutes / 60);
+                    const minutes = totalMinutes % 60;
+                    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
                 }
                 const rangeRef = sheet['!ref'];
                 if (rangeRef) {
@@ -223,7 +231,8 @@ export default defineComponent({
                     for (let colIndex = 0; colIndex < cols; colIndex++) {
                         const col = XLSX.utils.encode_col(colIndex); // Convierte el índice en una columna: 0 -> "A", 1 -> "B"
                         
-                        const rowTrenes = sheetName.toLowerCase().includes('univ') ? 4 : 1
+                        // const rowTrenes = sheetName.toLowerCase().includes('univ') ? 4 : 1
+                        const rowTrenes = 1
                         const trenNro = `${col}${rowTrenes}`; // Busco el numero de tren
                         const cellTren = sheet[trenNro];
                         
@@ -234,6 +243,9 @@ export default defineComponent({
                                 tren: cellTren.w,
                                 itinerario: this.itinerario,
                                 circular: this.circular,
+                                rotacion: sheet[col+rowRotacion]?.w||'',
+                                trenSiguiente: sheet[col+rowTrenSiguiente]?.w||'',
+                                trenSiguienteHora: formatoHora(sheet[col+rowTrenSiguienteHora]?.v)||'',
                                 horarioXEst: {},
                                 horarios: [],
                                 estaciones: [],
@@ -244,12 +256,9 @@ export default defineComponent({
                                 const cell = sheet[cellAddress];
                                 
                                 if(cell && typeof cell.v === 'number' && cell.v <= 1){
-                                    // Convertir el número a horas y minutos
-                                    const totalMinutes = Math.round(cell.v * 24 * 60); // Convertir días a minutos
-                                    const hours = Math.floor(totalMinutes / 60);
-                                    const minutes = totalMinutes % 60;
                                     
-                                    const formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+                                    // Convertir el número a horas y minutos
+                                    const formattedTime = formatoHora(cell.v);
                                     
                                     nuevoItinerario.horarios.push(formattedTime);
                                     nuevoItinerario.estaciones.push(estaciones[i]);
@@ -264,50 +273,87 @@ export default defineComponent({
             this.lstItinerariosTrenes = Object.values(this.itinerariosTrenes)
             // console.log(this.itinerariosTrenes)
         },
-        obtenerItEstRows(sheetName:string): { rows: number[]; estaciones: string[] }|undefined{
+        obtenerItEstRows(sheetName:string): 
+            { rows: number[]; estaciones: string[];rowRotacion:number;rowTrenSiguiente:number,rowTrenSiguienteHora:number; }|undefined{
             let rows:Array<number> = []
             let estaciones:Array<string> = []
+            let rowRotacion:number = 0;
+            let rowTrenSiguiente: number = 0;
+            let rowTrenSiguienteHora: number = 0;
 
             if(sheetName.toLowerCase().includes('asc')&&sheetName.toLowerCase().includes('ty')){
                 estaciones = ['PC','ALL','K5','RE','TY','BO','GW','AK','CY','FV','BQ','GT','LLV','MG','ZZ']
                 rows = [4,6,8,11,14,16,18,20,23,25,27,29,31,33,35]
+                rowRotacion = 36;
+                rowTrenSiguiente = 37;
+                rowTrenSiguienteHora = 38
             }else if(sheetName.toLowerCase().includes('des')&&sheetName.toLowerCase().includes('ty')){
                 estaciones = ['PC','ALL','RE','K5','TY','BO','GW','AK','CY','FV','BQ','GT','LLV','MG','ZZ'].reverse()
                 rows = [4,6,8,10,12,14,16,19,21,23,25,28,31,33,35]
+                rowRotacion = 36;
+                rowTrenSiguiente = 37;
+                rowTrenSiguienteHora = 38
             }else if(sheetName.toLowerCase().includes('asc')&&sheetName.toLowerCase().includes('ql')){
                 estaciones = ['PC','ALL','QL','BZ','BQ','HD','VE','OA','LP']
                 rows = [3,5,11,13,18,20,22,26,27]
+                rowRotacion = 28;
+                rowTrenSiguiente = 29;
+                rowTrenSiguienteHora = 30
             }else if(sheetName.toLowerCase().includes('des')&&sheetName.toLowerCase().includes('ql')){
                 estaciones = ['PC','ALL','QL','BZ','BQ','HD','VE','OA','LP'].reverse()
                 rows = [3,4,8,10,12,17,19,25,27]
-            }
-            else if(sheetName.toLowerCase().includes('asc')&&sheetName.toLowerCase().includes('hdo')){
+                rowRotacion = 28;
+                rowTrenSiguiente = 29;
+                rowTrenSiguienteHora = 30
+            }else if(sheetName.toLowerCase().includes('asc')&&sheetName.toLowerCase().includes('hdo')){
                 estaciones = ['RE','TY','K34','SJ','HDO']
-                rows = [3,6,11,16,18]                
+                rows = [3,6,11,16,18]
+                rowRotacion = 20;
+                rowTrenSiguiente = 21;
+                rowTrenSiguienteHora = 22
             }else if(sheetName.toLowerCase().includes('des')&&sheetName.toLowerCase().includes('hdo')){
                 estaciones = ['RE','TY','K34','SJ','HDO'].reverse()
-                rows = [3,5,10,15,18]                
-            }
-            else if(sheetName.toLowerCase().includes('asc')&&sheetName.toLowerCase().includes('cñ')){
+                rows = [3,5,10,15,18] 
+                rowRotacion = 20;
+                rowTrenSiguiente = 21;               
+                rowTrenSiguienteHora = 22
+            }else if(sheetName.toLowerCase().includes('asc')&&sheetName.toLowerCase().includes('cñ')){
                 estaciones = ['PC','RE','TY','LLV','MG','ZZ','TZ','VC','CÑ','MN','LB']
-                rows = [3,10,13,15,17,19,21,24,29,33,36]                
+                rows = [3,10,13,15,17,19,21,24,29,33,36] 
+                rowRotacion = 38;
+                rowTrenSiguiente = 39;               
+                rowTrenSiguienteHora = 40
             }else if(sheetName.toLowerCase().includes('des')&&sheetName.toLowerCase().includes('cñ')){
                 estaciones = ['PC','RE','TY','LLV','MG','ZZ','TZ','VC','CÑ','MN','LB'].reverse()
-                rows = [3,6,10,15,18,20,22,24,26,29,36]                
-            }
-            else if(sheetName.toLowerCase().includes('asc')&&sheetName.toLowerCase().includes('chc')){
+                rows = [3,6,10,15,18,20,22,24,26,29,36]      
+                rowRotacion = 38;
+                rowTrenSiguiente = 39;          
+                rowTrenSiguienteHora = 40
+            }else if(sheetName.toLowerCase().includes('asc')&&sheetName.toLowerCase().includes('chc')){
                 estaciones = ['PC','RE','TY','BO','GW','AK','D','BA','AO','CHC']
-                rows = [4,10,13,15,17,19,20,21,23,25]                
+                rows = [4,10,13,15,17,19,20,21,23,25]   
+                rowRotacion = 27;
+                rowTrenSiguiente = 28;             
+                rowTrenSiguienteHora = 29
             }else if(sheetName.toLowerCase().includes('des')&&sheetName.toLowerCase().includes('chc')){
                 estaciones = ['PC','RE','TY','BO','GW','AK','D','BA','AO','CHC'].reverse()
-                rows = [4,6,8,9,10,12,14,16,19,25]                
+                rows = [4,6,8,9,10,12,14,16,19,25] 
+                rowRotacion = 27;
+                rowTrenSiguiente = 28;               
+                rowTrenSiguienteHora = 29
             }
             else if(sheetName.toLowerCase().includes('asc')&&sheetName.toLowerCase().includes('univ')){
                 estaciones = ['LP','ARQ','INF','MED','PER','D73','POL','AV7','CIR','MER','HDIOS']
                 rows = [3,4,5,6,7,8,9,10,11,12,13]    
+                rowRotacion = 14;
+                rowTrenSiguiente = 15;
+                rowTrenSiguienteHora = 16
             }else if(sheetName.toLowerCase().includes('des')&&sheetName.toLowerCase().includes('univ')){
                 estaciones = ['LP','ARQ','INF','MED','PER','D73','POL','AV7','CIR','MER','HDIOS'].reverse()
-                rows = [3,4,5,6,7,8,9,10,11,12,13]    
+                rows = [3,4,5,6,7,8,9,10,11,12,13]  
+                rowRotacion = 14;
+                rowTrenSiguiente = 15;  
+                rowTrenSiguienteHora = 16
             }else{
                 alert(`El sistema no logra discernir el ramal. Para la hoja ${sheetName} y no puede continuar con la carga.`)
                 return 
@@ -327,7 +373,7 @@ Por favor introducir H para Hábil, S para Sábado o D para Domingo. en Mayúscu
                 this.itinerario =  respuesta
 
             }
-            return {rows,estaciones}
+                return {rows, estaciones ,rowRotacion ,rowTrenSiguiente,rowTrenSiguienteHora}
         },
         handleRequestError(error: AxiosError) {
             console.error("Error en la solicitud:", error);
